84d542dce0b07c180a71f7756bef561efd98d5e3
baconYao <bacon735392@gmail.com>
2016-12-02 00:09:12 +0800

    final version

---
+++ lab11/ex1-1/SynchronizedCounter2.java
@@ -0,0 +1,50 @@
+/*
+  Another write method
+  Refference: http://www.cnblogs.com/GnagWang/archive/2011/02/27/1966606.html
+              https://docs.oracle.com/javase/tutorial/essential/concurrency/runthread.html
+*/
+
+public class SynchronizedCounter2{
+  public int c = 0;
+
+  public void Inc() {
+    for(int i = 0;i<25000000;i++){
+      c++;
+    }
+  }
+
+  public void Dec() {
+    for(int i = 0;i<25000000;i++){
+      c--;
+    }
+  }
+    
+  
+  public int getValue(){
+    return c;
+  }
+
+  public static void main(String[] args){
+    
+    final SynchronizedCounter2 Syc = new SynchronizedCounter2();
+    Thread thread0 = new Thread(new Runnable() {  public void run() {  Syc.Inc();}});
+    thread0.start();
+    Thread thread1 = new Thread(new Runnable() {  public void run() {  Syc.Dec();}});
+    thread1.start();
+    Thread thread2 = new Thread(new Runnable() {  public void run() {  Syc.Inc();}});
+    thread2.start();  
+    Thread thread3 = new Thread(new Runnable() {  public void run() {  Syc.Dec();}});
+    thread3.start();
+
+    try{
+      thread0.join();
+      thread1.join();
+      thread2.join();
+      thread3.join();
+    } catch(InterruptedException e) {
+      //do nothing
+    }
+    System.out.printf("(%d)%n", Syc.getValue());
+  }
+
+}
