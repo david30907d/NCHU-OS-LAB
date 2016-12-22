06a6a5df0d8205e1ec2856c44e08578c04f50e04
Pei Yao-Chung <baconYao@users.noreply.github.com>
2016-12-22 23:32:32 +0800

    Update ex2.c

--- lab14/ex2.c
+++ lab14/ex2.c
@@ -1,10 +1,10 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<sys/mman.h>
-#include<sys/stat.h>
-#include<unistd.h>
-#include<fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
 #define FILE_LENGTH 0x50
 
 int main(int argc ,char *argv[])
@@ -21,7 +21,7 @@ int main(int argc ,char *argv[])
         close(fd);
 
         printf("data from [%d] to [%d] is :",start,end);
-        for(i = start-1;i < end;i++)
+        for(i = start-1;i <= end;i++)
         {
                 printf("%c",*(map_memory+i));
         }
